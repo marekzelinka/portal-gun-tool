daca572d3c842de7fb5d436ab08cc5adad256600
Marek Zelinka <mzelinka17@gmail.com>
2026-03-06 11:40:49 +0100

    types

--- tests/test_gun.py
+++ tests/test_gun.py
@@ -3,14 +3,14 @@ import typer.testing
 from portal_gun_tool.main import app
 
 
-def test_reload_command(runner: typer.testing.CliRunner):
+def test_reload_command(runner: typer.testing.CliRunner) -> None:
     result = runner.invoke(app, ["gun", "reload"])
 
     assert result.exit_code == 0
     assert "Reloading portal gun" in result.stdout
 
 
-def test_shoot_command(runner: typer.testing.CliRunner):
+def test_shoot_command(runner: typer.testing.CliRunner) -> None:
     result = runner.invoke(app, ["gun", "shoot"])
 
     assert result.exit_code == 0
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,14 +1,30 @@
+import unittest.mock
+
+import pytest
 import typer.testing
 
 from portal_gun_tool import app
+from portal_gun_tool.main import version_callback
 
 
-def test_version_callback(runner: typer.testing.CliRunner):
-    result = runner.invoke(app, ["--version"])
+def test_version_option(runner: typer.testing.CliRunner) -> None:
+    with (
+        unittest.mock.patch("portal_gun_tool.main.__project_name__", new="fake-tool"),
+        unittest.mock.patch("portal_gun_tool.main.get_version") as mocked_version,
+    ):
+        mocked_version.return_value = "9.9.9-alpha"
 
-    assert result.exit_code == 0
-    assert "portal_gun_tool version" in result.stdout
+        result = runner.invoke(app, ["--version"])
 
-    import re
+        assert result.exit_code == 0
+        assert "fake-tool version 9.9.9-alpha" in result.stdout
 
-    assert re.search(r"\d+\.\d+\.\d+", result.stdout)
+        # Ensure the function was actually called once
+        mocked_version.assert_called_once()
+
+
+def test_version_callback() -> None:
+    assert version_callback(False) is None
+
+    with pytest.raises(typer.Exit):
+        version_callback(True)
